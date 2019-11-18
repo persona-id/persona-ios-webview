08380714c6032d3e11f96a7ca5b5bc23787b9be9
Paulo Fierro <paulo@paulofierro.com>
2019-11-18 13:30:23 -0500

    Removed unnecessary code

--- wkwebview/ViewController.swift
+++ wkwebview/ViewController.swift
@@ -6,17 +6,7 @@
 //
 
 import UIKit
-import WebKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    // Intentionally left blank.
 }
