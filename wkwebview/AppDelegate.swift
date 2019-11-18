2c1fd2333ddd1c30ecccb0caaf1a193937f54b50
Paulo Fierro <paulo@paulofierro.com>
2019-11-18 14:26:06 -0500

    Removed unnecessary image picker code

--- wkwebview/AppDelegate.swift
+++ wkwebview/AppDelegate.swift
@@ -8,19 +8,9 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UIImagePickerControllerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        picker.sourceType = .camera
-        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            // imageViewPic.contentMode = .scaleToFill
-            //imageViewPic.image = pickedImage
-            print(pickedImage.size)
-        }
-        picker.dismiss(animated: true, completion: nil)
-    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
